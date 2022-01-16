823a26ef891351ed7b9c35a25215f05201e8c0bc
SuperuserabT <SuperuserabT@gmail.com>
2022-01-16 12:46:18 -0500

    Added Configurations

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyA1FNAR_OF1qdZpsv7p-gcX-VIOFt1a048",
+        authDomain: "team-voting-app-dd28a.firebaseapp.com",
+        databaseURL: "https://team-voting-app-dd28a-default-rtdb.firebaseio.com",
+        projectId: "team-voting-app-dd28a",
+        storageBucket: "team-voting-app-dd28a.appspot.com",
+        messagingSenderId: "956425294712",
+        appId: "1:956425294712:web:001cff2d8947510c3dee8d"
+      };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
